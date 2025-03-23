b0f0b3da6aa656fa9113fbb0a949a052c7955101
marcelojb <marcelojb@rgm.com.br>
2025-03-23 18:01:40 -0300

    Versão inicial

--- demo/__manifest__.py
+++ demo/__manifest__.py
@@ -1,22 +1,22 @@
 {
     'name' : 'demo',
     'version' : '1.0',
-    'description' : 'demo - description *',
-    'summary' : 'demo - summary',
+    'description' : 'demo - description',
+    'summary' : 'demo - summary :)',
     'author' : 'demo - author - badarov',
 
     'category': 'Demo/Demo',
-
-    "depends" : [
-        "sale",
-        "account"
-    ],
+   
+    #"depends" : [
+    #    "sale",
+    #    "account"
+    #],
 
     "data" : [
         "views/demo_actions.xml",
-        "views/demo_menus.xml",
         "security/demo_security.xml",
-        #"security/ir.model.access.csv"        
+        "views/demo_menus.xml",
+        "security/ir.model.access.csv"        
     ],
     
     # para definir se o módulo é um application 
