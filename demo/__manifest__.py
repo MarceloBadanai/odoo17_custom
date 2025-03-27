3086a6878c372928e2b8eba8e385f90b8773a088
marcelojb <marcelojb@rgm.com.br>
2025-03-27 18:04:36 -0300

    Relatório funcionando

--- demo/__manifest__.py
+++ demo/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'demo',
-    'version' : '1.0',
+    'version' : '1.1',
     'description' : 'demo - description',
     'summary' : 'demo - summary :)',
     'author' : 'demo - author - badarov',
